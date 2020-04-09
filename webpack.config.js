34962387bf85cf795b8a36861dd749bd44f39cc5
DonokaMark-2016 <donokamark@gmail.com>
2020-04-09 10:07:05 +0800

    起步->结论

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
+
+//使用配置文件进行构建npx webpack --config webpack.config.js
