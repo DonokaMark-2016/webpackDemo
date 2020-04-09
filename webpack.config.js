ce2c88fb3232097d3d45c20b3c4efbea46bee569
DonokaMark-2016 <donokamark@gmail.com>
2020-04-09 10:44:57 +0800

    回退处理

--- webpack.config.js
+++ webpack.config.js
@@ -6,41 +6,4 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  module:{
-  	rules:[
-	  	{
-				test: /\.css$/,
-				use: [
-					'style-loader',
-					'css-loader'
-				]
-	  	},
-	  	{
-	  		test:/\.(png|svg|jpg|gif)$/,
-	  		use:[
-					'file-loader'
-	  		]
-	  	},
-	  	{
-	  		test:/\.(woff|woff2|eot|ttf|otf)$/,
-	  		use:[
-					'file-loader'
-	  		]
-	  	},
-	  	{
-	  		test:/\.(csv|tsv)$/,
-	  		use:[
-					'csv-loader'
-	  		]
-	  	},
-	  	{
-	  		test:/\.xml$/,
-	  		use:[
-					'xml-loader'
-	  		]
-	  	}
-  	]
-  }
 };
-
-//使用配置文件进行构建npx webpack --config webpack.config.js
